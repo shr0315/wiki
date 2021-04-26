d2df3acad5ea4c4da3dcc79cf28d61a5eecf1fb0
shr0315 <shr1004504975>
2021-04-26 16:29:07 +0800

    3-4 mabatis使用实例：开发、test/list接口，查询test表所有数据

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController //返回字符串
 //@Controller  返回页面
@@ -11,6 +17,9 @@ public class TestController {
 
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -20,5 +29,10 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "hello world post" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
