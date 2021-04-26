b1c8d75167fec9a1a0dbf90c0974e24349217fa2
shr0315 <shr1004504975>
2021-04-26 11:48:09 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller  返回页面
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + testHello;
     }
 
     @PostMapping("/hello/post")
