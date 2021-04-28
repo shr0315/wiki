9813fec60301fd2b21d9a09839dd1f20251c75b4
shr0315 <shr1004504975>
2021-04-28 16:06:38 +0800

    5-4 电子书布局调整，一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -47,35 +47,21 @@
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
 
 
-  <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-    <template #footer>
-      <div>
-        <b>ant design vue</b>
-        footer part
-      </div>
-    </template>
+  <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3}" :data-source="ebooks">
     <template #renderItem="{ item }">
-      <a-list-item key="item.title">
+      <a-list-item key="item.name">
         <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
         </template>
-        <template #extra>
-          <img
-            width="272"
-            alt="logo"
-            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-          />
-        </template>
         <a-list-item-meta :description="item.description">
           <template #title>
-            <a :href="item.href">{{ item.title }}</a>
+            <a :href="item.href">{{ item.name }}</a>
           </template>
-          <template #avatar><a-avatar :src="item.avatar" /></template>
+          <template #avatar><a-avatar :src="item.cover" /></template>
         </a-list-item-meta>
-        {{ item.content }}
       </a-list-item>
     </template>
   </a-list>
@@ -106,11 +92,6 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
-  // components: {
-  //   StarOutlined,
-  //   LikeOutlined,
-  //   MessageOutlined,
-  // },
   setup(){
     const pagination = {
       onChange: (page: any) => {
