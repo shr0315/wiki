e9aefc0edf3ff55a7e9fcfd039d4083f0f911d3a
shr0315 <shr1004504975>
2021-04-29 16:10:00 +0800

    6-3 电子书管理表格展示数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -2,9 +2,151 @@
   <a-layout>
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-      <div class="about">
-        <h1>电子书管理</h1>
-      </div>
+
+
+
+      <a-table
+              :columns="columns"
+              :row-key="record => record.id"
+              :data-source="ebooks"
+              :pagination="pagination"
+              :loading="loading"
+              @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar" />
+        </template>
+
+        <template v-slot:category="{ text, record }">
+          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <!--<router-link :to="'/admin/doc?ebookId=' + record.id">-->
+            <!--  <a-button type="primary">-->
+            <!--    文档管理-->
+            <!--  </a-button>-->
+            <!--</router-link>-->
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <!--<a-popconfirm-->
+            <!--        title="删除后不可恢复，确认删除?"-->
+            <!--        ok-text="是"-->
+            <!--        cancel-text="否"-->
+            <!--        @confirm="handleDelete(record.id)"-->
+            <!--&gt;-->
+              <a-button type="danger">
+                删除
+              </a-button>
+            <!--</a-popconfirm>-->
+          </a-space>
+        </template>
+      </a-table>
+
+
+
     </a-layout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from 'vue';
+  import axios from 'axios';
+
+  export default defineComponent({
+    name:'AdminEbook',
+    setup(){
+      const ebooks = ref();
+      const pagination = ref({
+        current: 1,
+        pageSize: 2,
+        total: 0
+      });
+      const loading = ref(false);
+
+      const columns = [
+        {
+          title: '封面',
+          dataIndex: 'cover',
+          slots: { customRender: 'cover' }
+        },
+        {
+          title: '名称',
+          dataIndex: 'name'
+        },
+        {
+          title: '分类一',
+          key: 'category1Id',
+          dataIndex: 'category1Id',
+        },
+        {
+          title: '分类二',
+          dataIndex: 'category2Id',
+        },
+        {
+          title: '文档数',
+          dataIndex: 'docCount'
+        },
+        {
+          title: '阅读数',
+          dataIndex: 'viewCount'
+        },
+        {
+          title: '点赞数',
+          dataIndex: 'voteCount'
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          slots: { customRender: 'action' }
+        }
+      ];
+
+      /**
+       * 数据查询
+       */
+      const handleQuery = (params: any) => {
+        loading.value = true;
+        axios.get("/ebook/list",params).then((response) => {
+          loading.value = false;
+          const data = response.data;
+          ebooks.value = data.content;
+
+        //  重置分页按钮
+          pagination.value.current = params.page;
+        });
+      };
+
+      /**
+       * 表格点击页码时触发
+       */
+      const handleTableChange = (pagination: any) => {
+        console.log("看看自带的分页参数都有什么："+ pagination);
+        handleQuery({
+          page:pagination.current,
+          size:pagination.pageSize
+        });
+      };
+
+      onMounted(()=>{
+        handleQuery({});
+      });
+
+      return{
+        ebooks,
+        pagination,
+        columns,
+        loading,
+        handleTableChange
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  img {
+    width: 50px;
+    height: 50px;
+  }
+</style>
