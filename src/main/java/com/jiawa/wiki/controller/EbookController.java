d818ffcac4ff75b76c3894dabd9c60055f6fbeeb
shr0315 <shr1004504975>
2021-04-30 14:53:09 +0800

    6-11 集成Validation，为列表接口增加参数校验

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RequestMapping("/ebook")
 @RestController //返回字符串
@@ -19,7 +20,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req){
+    public CommonResp list(@Valid EbookQueryReq req){
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -83,6 +83,7 @@
 <script lang="ts">
   import { defineComponent, onMounted, ref } from 'vue';
   import axios from 'axios';
+  import { message } from "ant-design-vue";
 
   export default defineComponent({
     name:'AdminEbook',
@@ -90,7 +91,7 @@
       const ebooks = ref();
       const pagination = ref({
         current: 1,
-        pageSize: 4,
+        pageSize: 4000,
         total: 0
       });
       const loading = ref(false);
@@ -146,11 +147,14 @@
         }).then((response) => {
           loading.value = false;
           const data = response.data;
-          ebooks.value = data.content.list;
-
-        //  重置分页按钮
-          pagination.value.current = params.page;
-          pagination.value.total = data.content.total;
+          if(data.success){
+            ebooks.value = data.content.list;
+            //  重置分页按钮
+            pagination.value.current = params.page;
+            pagination.value.total = data.content.total;
+          }else{
+            message.error(data.message);
+          }
         });
       };
 
