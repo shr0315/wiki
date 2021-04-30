5029e74d72f6eecad2d211514ce2fbf0fea83c03
shr0315 <shr1004504975>
2021-04-30 14:35:02 +0800

    6-10 增加删除电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -34,16 +34,16 @@
             <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
-            <!--<a-popconfirm-->
-            <!--        title="删除后不可恢复，确认删除?"-->
-            <!--        ok-text="是"-->
-            <!--        cancel-text="否"-->
-            <!--        @confirm="handleDelete(record.id)"-->
-            <!--&gt;-->
+            <a-popconfirm
+                    title="删除后不可恢复，确认删除?"
+                    ok-text="是"
+                    cancel-text="否"
+                    @confirm="handleDelete(record.id)"
+            >
               <a-button type="danger">
                 删除
               </a-button>
-            <!--</a-popconfirm>-->
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
@@ -202,6 +202,22 @@
         ebook.value = {};
       };
 
+      /**
+       * 删除
+       * @param id
+       */
+      const handleDelete = (id:number) =>{
+        axios.delete("/ebook/delete/"+id).then((response) => {
+          const data = response.data;  //data = commonResp
+          if(data.success){
+            //重新加载列表
+            handleQuery({
+              page:pagination.value.current,
+              size:pagination.value.pageSize,
+            });
+          }
+        });
+      };
 
 
       onMounted(()=>{
@@ -220,6 +236,7 @@
 
         edit,
         add,
+        handleDelete,
 
         ebook,
         modalVisible,
